c06189ed01fefc492e96ddbeccfa8021093c7b60
theresearchlab <aarontellis1996@outlook.com>
2023-06-26 20:19:19 -0400

    add views for tablet and phone plus open and close navbar function

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+
+menuBtn.addEventListener('click', ()=> {
+    sideMenu.style.display = 'block';
+})
+
+closeBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'none';
+})
+
+console.log('hello');
